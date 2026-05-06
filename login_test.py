697cf31e9f15dd82be8cb63d1dc94feab20526a8
Shubham Vairagade <Vairagades@Gmail.com>
2026-05-06 18:24:09 +0530

    Added login automation test script

--- login_test.py
+++ login_test.py
@@ -1,1 +1,8 @@
-Login test script 
+def test_login():
+    username = "test_user"
+    password = "123456"
+
+    if username == "test_user" and password == "123456":
+        print("Login Passed")
+    else:
+        print("Login Failed")Login test script 
